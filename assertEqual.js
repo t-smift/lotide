b126a226c6a0a1a93f00a9e0a4e5dade4ef83d92
Taylor Smith <taylor.l.smith07@gmail.com>
2022-09-19 14:35:30 -0700

    first function in lotide library, to compare to values

---
+++ assertEqual.js
@@ -0,0 +1,8 @@
+const assertEqual = function(actual, expected) {
+ if (actual === expected){
+  console.log("👏👏👏 Assertion Passed: " + actual + " === " + expected);
+ } else console.log("❌ ❌ ❌ Assertion Failed: " + actual + " !== " + expected);
+}
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
