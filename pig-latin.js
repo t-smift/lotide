a8a9eafe6e54cc19d4b3bb4d46efe5ba1b411397
Taylor Smith <taylor.l.smith07@gmail.com>
2022-09-19 17:06:45 -0700

    function similar to reverse but with pig latin strings

---
+++ pig-latin.js
@@ -0,0 +1,13 @@
+const args = process.argv;
+const pigString = function(str) {
+  let pigLatin = ""
+  let tail = str.slice(1)
+  pigLatin = tail + str[0] + "ay"
+  return pigLatin;
+};
+const pigLatin = function(array) {
+  for (x = 2; x < array.length; x++) {
+    console.log(pigString(array[x]));
+  }
+};
+console.log(pigLatin(args))
