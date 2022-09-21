655e4c71bd8e39ee9cfa805c6143478e8969f668
Taylor Smith <taylor.l.smith07@gmail.com>
2022-09-20 19:20:32 -0700

    fixed function to work test cases

--- without.js
+++ without.js
@@ -17,9 +17,12 @@ const assertArraysEqual = function(first, second) {
 
 const without = function(source, itemsToRemove) {
   let result = []
-  for (x = 0; x < source.length; x++) {
-    if (source[x] !== itemsToRemove[x]) {
-      result.push(source[x])
+  result = result.concat(source);
+  for (let x = 0; x < source.length; x++) {
+    for (let y = 0; y < itemsToRemove.length; y++) {
+      if (result[x] === itemsToRemove[y]) {
+        result.splice(x, 1);
+      }
     }
   }
   return result
@@ -27,3 +30,6 @@ const without = function(source, itemsToRemove) {
 console.log(without(["1", "2", "3"], [1, "2", "3"]))
 const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]);
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+assertArraysEqual(without([1, 2, 3, 1, 2, 3], [1, 2]), [3, 3]);
+assertArraysEqual(without([1, 2, 3, 1, 2, 3], [1]), [2, 3, 2, 3]);
