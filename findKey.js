224bd2e500a6efff901e9345638e89073a0236ff
Taylor Smith <taylor.l.smith07@gmail.com>
2022-09-22 16:47:02 -0700

    function to return a key when the value matches a condition

---
+++ findKey.js
@@ -0,0 +1,39 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👏👏👏 Assertion Passed: ${actual} === ${expected}`);
+  } else console.log(`❌ ❌ ❌ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const findKey = function(object, callback) {
+  let result = "";
+  for (key in object) {
+    if (callback(object[key])){
+      result = key;
+      break;
+    }
+  }
+  return result;
+};
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)); // => "noma"
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
+assertEqual(findKey({
+  "John": { adult: false},
+  "Todd": { adult: false},
+  "Annie": { adult: true},
+  "Sally": { adult: false}
+}, x => x.adult === true), "Annie");
