9789ab64480dfdd40869f970398d030ace2d86c8
Taylor Smith <taylor.l.smith07@gmail.com>
2022-09-26 13:49:47 -0700

    simplified my middleTest to no longer include assertEqual as well

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,8 @@
 const assertArraysEqual = require('../assertArraysEqual');
-const assertEqual = require('../assertEqual');
 const middle = require('../middle');
 
-assertEqual(assertArraysEqual(middle([1]),[]), true);
-assertEqual(assertArraysEqual(middle([]),[]), true);
-assertEqual(assertArraysEqual(middle([1, 2, 3]), 2), true);
-assertEqual(assertArraysEqual(middle([1, 2, 3, 4, 5]), 3), true);
-assertEqual(assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]), true);
+assertArraysEqual(middle([1]),[]);
+assertArraysEqual(middle([]),[]);
+assertArraysEqual(middle([1, 2, 3]), 2);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), 3);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]);
