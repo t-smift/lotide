78827df5e716c3e52e824f3fbc3d74ca06af2acd
Taylor Smith <taylor.l.smith07@gmail.com>
2022-09-26 13:55:04 -0700

    refactored files to move test functions into new directory

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,5 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length != arr2.length) {
-    return false
-  }
-  for (x = 0; x < arr1.length; x++) {
-    if (arr1[x] !== arr2[x]) {
-      return false
-    }
-  }
-  return true
-}
+const eqArrays = require('./eqArrays');
+
 
 const assertArraysEqual = function(first, second) {
   if (eqArrays(first, second)) {
@@ -16,6 +7,5 @@ const assertArraysEqual = function(first, second) {
   } else console.log(`❌ ❌ ❌ Assertion Failed: ${first} !== ${second}`);
 }
 
-console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]))
-console.log(assertArraysEqual([1, 2, 3], [3, 2, 1]))
-console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", 3]))
+
+module.exports = assertArraysEqual
--- middle.js
+++ middle.js
@@ -1,22 +1,3 @@
-
-var eqArrays = function(arr1, arr2) {
-  if (arr1.length != arr2.length) {
-    return false
-  }
-  for (x = 0; x < arr1.length; x++) {
-    if (arr1[x] !== arr2[x]) {
-      return false
-    }
-  }
-  return true
-} 
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`👏👏👏 Assertion Passed: ${actual} === ${expected}`);
-  } else console.log(`❌ ❌ ❌ Assertion Failed: ${actual} !== ${expected}`);
-}
-
 const middle = function(array) {
   let midEle = [];
   let chars = array.length
@@ -31,8 +12,5 @@ const middle = function(array) {
   }
   return midEle;
 };
-console.log(middle([1]));
-console.log(middle([]));
-console.log(middle([1, 2, 3]));
-console.log(middle([1, 2, 3, 4, 5]));
-console.log(middle([1, 2, 3, 4, 5, 6, 7, 8]));
+
+module.exports = middle;
